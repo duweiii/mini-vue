bada2919858cdb6f6bd47a3e0b0c18279b2e520b
duweiii <15106997058@163.com>
2022-06-18 16:18:28 +0800

    feat: runtime-core >  mountElement

--- example/app.js
+++ example/app.js
@@ -2,7 +2,13 @@ import { h } from '../lib/guide-mini-vue.esm.js';
 
 export const App = {
   render(){
-    return h('div','print word' + this.msg)
+    return h('div',
+    {
+      id: 'root',
+      class: 'root-class'
+    },
+    [h('div',{class: 'red'}, 'hi'), h('p', {class: 'blue'}, 'nihao')]
+    )
   },
   setup(){
     return {
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container){
@@ -5,8 +6,38 @@ export function render(vnode, container){
 }
 
 export function patch(vnode, container){
-  
-  propcessComponent(vnode, container)
+  if(typeof vnode.type === 'string'){
+    processElement(vnode, container)
+  }else if ( isObject(vnode.type) ){
+    propcessComponent(vnode, container)
+  }
+}
+
+function processElement(vnode, container){
+  mountElement(vnode, container)
+}
+function mountElement(vnode, container){
+  let el = document.createElement(vnode.type);
+
+  const { props } = vnode;
+
+  for (const attr in props) {
+    const value = props[attr];
+    el.setAttribute(attr, value)
+  }
+
+  const { children } = vnode;
+  if( typeof children === 'string' ){
+    el.innerText = children;
+  }else if (Array.isArray(children)){
+    mountChildren(children, el)
+  }
+
+  container.append(el)
+}
+
+function mountChildren(children, container){
+  children.forEach(child => patch(child, container))
 }
 
 function propcessComponent(vnode, container){
