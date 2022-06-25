ea2f3fa1e8cc3b509c33148832229639cbfb8d4b
duweiii <15106997058@163.com>
2022-06-25 17:17:40 +0800

    feat: parse mustache

---
+++ src/compiler-core/src/parse.ts
@@ -0,0 +1,44 @@
+export function baseParse(content){
+  let context = createContext(content);
+  return createRoot( parseChildren(context) )
+}
+function createRoot(children){
+  return {
+    children
+  }
+}
+function createContext(content){
+  return {
+    source: content
+  }
+}
+function parseChildren(context){
+  let nodes: any = [];
+  let node;
+  if( context.source.startsWith("{{")){
+    node = parseInterpolation(context)
+  }
+  nodes.push(node)
+  return nodes;
+}
+function parseInterpolation(context){
+  let openDelimiter = "{{"
+  let closeDelimiter = "}}"
+  
+  const closeIndex = context.source.indexOf(closeDelimiter, openDelimiter.length)
+  advanced(context, openDelimiter.length)
+  let rawLength = closeIndex - openDelimiter.length;
+  let rawContent = context.source.slice(0, rawLength)
+  let content = rawContent.trim()
+  advanced(context, rawLength + closeDelimiter.length)
+  return {
+    type: 'interpolation',
+    content: {
+      type: 'simple_expression',
+      content: 'message'
+    }
+  }
+}
+function advanced(context, leng){
+  context.source = context.source.slice(leng)
+}
