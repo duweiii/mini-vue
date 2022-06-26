9cf6c94539b9423920ba20cbe89fc7265886c5d5
duweiii <15106997058@163.com>
2022-06-26 15:17:01 +0800

    feat: the interval contrast ( change order )

--- example/update-element-arrayToArray/app.js
+++ example/update-element-arrayToArray/app.js
@@ -54,6 +54,23 @@ import { h, ref } from '../../lib/guide-mini-vue.esm.js';
 // 下面就剩处理区间内的变化了 a,b,c,[d,e,f],g => abc[d,e,h]g
 // 包括，delete&changeOrder&create
 // 1. 删除
+// const oldChildren = [ 
+//   h('div', { key: "A" }, "A"),
+//   h('div', { key: "B" }, "B"),
+//   h('div', { key: "C" }, "C"),
+//   h('div', { key: "D" }, "D"),
+//   h('div', { key: "E" }, "E"),
+//   h('div', { key: "F" }, "F"),
+// ]
+// const newChildren = [ 
+//   h('div', { key: "A" }, "A"),
+//   h('div', { key: "B" }, "B"),
+//   h('div', { key: "E" }, "E"),
+//   h('div', { key: "C" }, "C"),
+//   h('div', { key: "F" }, "F"),
+// ]
+
+// 改变顺序
 const oldChildren = [ 
   h('div', { key: "A" }, "A"),
   h('div', { key: "B" }, "B"),
@@ -61,13 +78,16 @@ const oldChildren = [
   h('div', { key: "D" }, "D"),
   h('div', { key: "E" }, "E"),
   h('div', { key: "F" }, "F"),
+  h('div', { key: "G" }, "G"),
 ]
-const newChildren = [ 
+const newChildren = [
   h('div', { key: "A" }, "A"),
   h('div', { key: "B" }, "B"),
   h('div', { key: "E" }, "E"),
   h('div', { key: "C" }, "C"),
+  h('div', { key: "D" }, "D"),
   h('div', { key: "F" }, "F"),
+  h('div', { key: "G" }, "G"),
 ]
 
 
